573d3a6351ad4a52df2a7376ad1a74ff6586b921
auxk0rd03 <auxk0rdbusiness@gmail.com>
2025-03-01 14:25:03 -0500

    added token
    added token

--- API/WeatherApi.py
+++ API/WeatherApi.py
@@ -92,7 +92,7 @@ class NOAAWeather:
 if __name__ == "__main__":
     with open(".env","r"):
         
-        token = "ThisIsNotTheToken"
+        token = " "
         latitude = 39.7456  # Add in lat float from location data
         longitude = -97.0892  # Add in Long from location data
         weather = NOAAWeather(token)
